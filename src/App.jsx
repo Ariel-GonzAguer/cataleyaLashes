7fc5dfd0f58f82abbd4dfd8ce662be4c9812eeaf
ArieGonzAguer <ariegonzaguer@gmail.com>
2024-09-29 12:55:24 -0600

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './App.module.css';
+
+export default function App () {
+   
+   
+ return (
+   <section className={styles.homePage}>
+     <span>CL</span>
+     <h1>CATALEYA LASHES</h1>
+     <h2>BY MELISSA GONZÁLEZ</h2>
+   </section>
+ )
+}
